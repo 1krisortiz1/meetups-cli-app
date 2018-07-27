a8a3e8c37ead61718df6ef6bde39179152edc366
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-07-27 03:21:19 +0000

    cli

--- lib/area.rb
+++ lib/area.rb
@@ -1,4 +1,7 @@
 require "area/version"
+require 'nokogiri'
+require 'pry'
+require 'open uri'
 
 module Area
   # Your code goes here...
--- lib/area/cli.rb
+++ lib/area/cli.rb
@@ -1,6 +1,10 @@
 class LocalAreaMeetup
 
+  def call
   puts "test update"
+  end
 
+  def list_meetups
+
+  end
 end
- 
