059e6f1a81b57843a73dc1f675bdb02eebde3472
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-07-27 03:25:25 +0000

    code

--- lib/area/cli.rb
+++ lib/area/cli.rb
@@ -7,4 +7,18 @@ class LocalAreaMeetup
   def list_meetups
 
   end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter your selection or type exit:  "
+      input = gets.strip.downcase
+
+      if input.to_i > 0
+        the_meetup =
+  end
+
+  def goodbye
+    puts "See you tomorrow for more meetups!"
+  end
 end
